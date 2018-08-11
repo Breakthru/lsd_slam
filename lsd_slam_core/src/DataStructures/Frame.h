8d10c09a269c670f91676549d2c5e7cd2a502fb3
Marco Paladini <paladini.marco@gmail.com>
2018-08-11 16:52:20 +0100

    Small fixes for c++11

--- lsd_slam_core/src/DataStructures/Frame.h
+++ lsd_slam_core/src/DataStructures/Frame.h
@@ -160,8 +160,7 @@ public:
 		Eigen::aligned_allocator< Frame* > > neighbors;
 
 	/** Multi-Map indicating for which other keyframes with which initialization tracking failed.*/
-	std::unordered_multimap< Frame*, Sim3, std::hash<Frame*>, std::equal_to<Frame*>,
-		Eigen::aligned_allocator< std::pair<const Frame*,Sim3> > > trackingFailed;
+    std::unordered_multimap< Frame*, Sim3, std::hash<Frame*>, std::equal_to<Frame*> > trackingFailed;
 
 
 	// flag set when depth is updated.
